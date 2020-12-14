9bf36174d1cc7f263016b4aee71e3b1f7a29b9b7
h.ayabe <h.ayabe_biz@live.jp>
2020-12-14 21:13:58 +0900

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,32 +8,46 @@ void main() {
   runApp(const MyApp());
 }
 
+enum CounterEvent { decrement, reset }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(10);
+
+  void startCountDown() {
+    Timer.periodic(Duration(seconds: 1), (_) {
+      if (state == 0) {
+        this.add(CounterEvent.reset);
+      } else {
+        this.add(CounterEvent.decrement);
+      }
+    });
+  }
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.reset:
+        yield 10;
+        break;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Blob Timer Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: BlocProvider(create: (_) => CounterBloc(), child: MyHomePage()),
+      home: BlocProvider(
+          create: (_) => CounterBloc()..startCountDown(), child: MyHomePage()),
     );
-  }
-}
-
-enum CounterEvent { increment }
-
-class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0);
-
-  @override
-  Stream<int> mapEventToState(CounterEvent event) async* {
-    switch (event) {
-      case CounterEvent.increment:
-        yield state + 1;
-        break;
-    }
   }
 }
 
@@ -40,28 +56,58 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Blob Counter Sample'),
-      ),
-      body: Center(
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Blob Timer Sample'),
+            bottom: TabBar(tabs: const <Widget>[Text('Red'), Text('Blue')]),
+          ),
+          body: const TabBarView(children: <Widget>[
+            Red(),
+            Blue(),
+          ]),
+        ));
+  }
+}
+
+class Red extends StatelessWidget {
+  const Red({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Container(
+        color: Colors.red,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'This is "Red" Page',
             ),
             BlocBuilder<CounterBloc, int>(
-                builder: (context, state) => Text('$state',
+                builder: (context, state) => Text(
+                    state == 0 ? 'Bang!!!!' : '$state',
                     style: Theme.of(context).textTheme.headline2)),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            BlocProvider.of<CounterBloc>(context).add(CounterEvent.increment);
-          }),
-    );
-  }
+      );
+}
+
+class Blue extends StatelessWidget {
+  const Blue({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This is "Blue" Page',
+            ),
+            BlocBuilder<CounterBloc, int>(
+                builder: (context, state) => Text(
+                    state == 0 ? 'Boon!!!!' : '$state',
+                    style: Theme.of(context).textTheme.headline2)),
+          ],
+        ),
+      );
 }
