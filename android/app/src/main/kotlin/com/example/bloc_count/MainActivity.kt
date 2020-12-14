a86fd81609e93eba60b5731ffb1b12cbbfb20555
h.ayabe <h.ayabe_biz@live.jp>
2020-12-14 20:20:08 +0900

    bloc_count_sample

---
+++ android/app/src/main/kotlin/com/example/bloc_count/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_count
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider(create: (_) => CounterBloc(), child: MyHomePage()),
+    );
+  }
+}
+
+enum CounterEvent { increment }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+    }
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blob Counter Sample'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterBloc, int>(
+                builder: (context, state) => Text('$state',
+                    style: Theme.of(context).textTheme.headline2)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            BlocProvider.of<CounterBloc>(context).add(CounterEvent.increment);
+          }),
+    );
+  }
+}
